f0fa9450ecf2a4a5a23862037815be6479a72d1a
YuhaHabib <156275799+YuhaHabib@users.noreply.github.com>
2024-12-23 00:12:57 +0700

    Delete ml-api-exercise-main directory

--- ml-api-exercise-main/src/services/loadModel.js
+++
@@ -1,5 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-async function loadModel() {
-    return tf.loadGraphModel('https://storage.googleapis.com/bucketahmadsh/model.json');
-}
-module.exports = loadModel;
